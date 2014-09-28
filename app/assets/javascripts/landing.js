562b0877bd3caf9b87779bebba76d283b1c5fbd5
Zach Sherman <zksherm@gmail.com>
2014-09-27 20:55:41 -0400

    responsive header

---
+++ app/assets/javascripts/landing.js
@@ -0,0 +1,31 @@
+$(function() {
+
+  if(document.getElementById('large-header')) {
+    var width, height, largeHeader, target, animateHeader = true;
+    var navHeight = document.getElementById('top-nav').offsetHeight;
+
+    // Main
+      initHeader();
+      addListeners();
+
+    function initHeader() {
+      width = window.innerWidth;
+      height = window.innerHeight - 65 // Nav height;
+      target = {x: width/2, y: height/2};
+
+      largeHeader = document.getElementById('large-header');
+      largeHeader.style.height = height+'px';
+    }
+
+    function addListeners() {
+      window.addEventListener('resize', resize);
+    }
+
+    function resize() {
+      width = window.innerWidth;
+      height = window.innerHeight - 65 // Nav height;
+      largeHeader.style.height = height+'px';
+    }
+  }
+
+});
