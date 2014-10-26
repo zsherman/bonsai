302565d48d1159f2b6fa2c083c97d8ba51f4d166
Zach Sherman <zksherm@gmail.com>
2014-10-26 15:04:43 -0400

    some more hero styles

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,6 +14,7 @@ class Order < ActiveRecord::Base
 
     # Find or create records for each order
     # Todo: handle errors so correct orders still get executed
+    # Todo: handle case where user_id is nil i.e. for gift purchase
     begin
       orders.each do |o|
         order_attributes = o.note_attributes
