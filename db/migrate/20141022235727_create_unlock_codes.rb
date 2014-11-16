02e1e871ddaaac826c2f05428706aebfa4cd3c76
Zach Sherman <zksherm@gmail.com>
2014-11-16 12:43:11 -0500

    add svgs and style product header

--- db/migrate/20141022235727_create_unlock_codes.rb
+++ db/migrate/20141022235727_create_unlock_codes.rb
@@ -4,6 +4,8 @@ class CreateUnlockCodes < ActiveRecord::Migration
       t.references :user
       t.references :product
       t.string :code
+      t.integer :uses
+      t.integer :max_uses
       t.boolean :used, default: false
       t.timestamps
     end
