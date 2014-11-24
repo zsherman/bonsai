74f76badb4f1e5b28c458c4192a6bd77a0649202
Zach Sherman <zksherm@gmail.com>
2014-11-24 18:50:41 -0500

    add admin user

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,9 +1,14 @@
 class CreateAdminService
   def call
     user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
-        user.admin!
-      end
+      user.password = Rails.application.secrets.admin_password
+      user.password_confirmation = Rails.application.secrets.admin_password
+      user.admin!
+    end
+    user2 = User.find_or_create_by!(email: Rails.application.secrets.admin_2_email) do |user|
+      user2.password = Rails.application.secrets.admin_2_password
+      user2.password_confirmation = Rails.application.secrets.admin_2_password
+      user2.admin!
+    end
   end
 end
