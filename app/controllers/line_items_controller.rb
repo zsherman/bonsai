a30718ff14fbea75dccb16616c9f29ea21d9eba4
Zach Sherman <zksherm@gmail.com>
2014-11-29 12:58:10 -0500

    fix cart bug

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -25,7 +25,7 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     @cart = current_cart
-    product = Product.find(params[:product_id])
+    product = Product.friendly.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
