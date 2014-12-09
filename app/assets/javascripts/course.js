ac3d6b2c01cd4f8b2e6b99a8fb6342a2b29a6f8c
Zach Sherman <zksherm@gmail.com>
2014-12-08 20:05:27 -0500

    fix buttons

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -28,11 +28,15 @@ $(function() {
     }
 
     // Initialize first video
-    getVideo(course.lessons[0].video.url);
+    setTimeout(function() {
+      $('.lesson').first().click();
+    }, 1000)
 
     // Handle video clicks
     $('.lesson').on('click', function(e) {
       // Switch to that lesson
+      $('.selected').removeClass('selected');
+      $(this).addClass('selected');
       var videoUrl = $(this).data('video-url');
       var lessonTitle = $(this).data('lesson-title');
       var videoContent = $(this).data('video-content');
@@ -43,27 +47,29 @@ $(function() {
     });
 
     $('.next').on('click', function(e) {
+      $('.selected').next('.lesson').click();
       // Go to next lesson
-      currentLesson += 1;
-      var next = $("li[data-lesson-id='" + currentLesson +"']");
-      getVideo(next.data('video-url'));
-      var videoUrl = next.data('video-url');
-      var lessonTitle = next.data('lesson-title');
-      var videoContent = next.data('video-content');
-      $('.description-container h3').text(lessonTitle);
-      $('.description-container p').text(videoContent);
+      // currentLesson += 1;
+      // var next = $("li[data-lesson-id='" + currentLesson +"']");
+      // getVideo(next.data('video-url'));
+      // var videoUrl = next.data('video-url');
+      // var lessonTitle = next.data('lesson-title');
+      // var videoContent = next.data('video-content');
+      // $('.description-container h3').text(lessonTitle);
+      // $('.description-container p').text(videoContent);
     });
 
     $('.previous').on('click', function(e) {
+      $('.selected').prev('.lesson').click();
       // Go to next lesson
-      currentLesson -= 1;
-      var previous = $("li[data-lesson-id='" + currentLesson +"']");
-      getVideo(previous.data('video-url'));
-      var videoUrl = previous.data('video-url');
-      var lessonTitle = previous.data('lesson-title');
-      var videoContent = previous.data('video-content');
-      $('.description-container h3').text(lessonTitle);
-      $('.description-container p').text(videoContent);
+      // currentLesson -= 1;
+      // var previous = $("li[data-lesson-id='" + currentLesson +"']");
+      // getVideo(previous.data('video-url'));
+      // var videoUrl = previous.data('video-url');
+      // var lessonTitle = previous.data('lesson-title');
+      // var videoContent = previous.data('video-content');
+      // $('.description-container h3').text(lessonTitle);
+      // $('.description-container p').text(videoContent);
     });
 
 
